9ae90e3728d071d20236a1f1b8aee0a8a3bc543c
Marco-Rocco <marcorocco28@icloud.com>
2025-04-09 15:28:22 +0200

    feat: create function to use input form (uncontrolled)

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,24 @@ import articles from "../data/articles"
 
 
 function App() {
+  const [newItem, setNewItem] = useState("")
 
+  const handleChange = (event) => {
+    setNewItem(event.target.value);
+  }
   return (
     <>
+      <h1>panetteria stock</h1>
       <ul>
-        {articles.map((article) => <li>{article}</li>)}
+        {articles.map((article, index) => <li key={index}>{article}</li>)}
       </ul>
+
+      <form action="">
+        <input type="text" onChange={handleChange} value={newItem} />
+        <p>{newItem}</p>
+        <button>add new articles</button>
+      </form>
+
     </>
   )
 }
