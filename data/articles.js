05caa9381e5227249d05438120fd85009cdce7f2
Marco-Rocco <marcorocco28@icloud.com>
2025-04-09 15:37:04 +0200

    feat: create functioning button (not pushing item yet)

--- data/articles.js
+++ data/articles.js
@@ -1,3 +1,29 @@
-const articles = ['panini', 'rosette', 'pizzette', 'dolci']
+const articles = [
+    {
+        id: 1,
+        title: "pane",
+        description: "pane cafone"
+    },
+    {
+        id: 2,
+        title: "pizzette",
+        description: "pizzette al pomodoro"
+    },
+    {
+        id: 3,
+        title: "taralli",
+        description: "taralli tradizionali"
+    },
+    {
+        id: 4,
+        title: "dolci",
+        description: "dolcetti sfusi"
+    },
+    {
+        id: 5,
+        title: "rosette",
+        description: "pane rosetta"
+    },
+]
 
 export default articles
--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,23 @@ import articles from "../data/articles"
 function App() {
   const [newItem, setNewItem] = useState("")
 
-  const handleChange = (event) => {
+  const handleChange = event => {
     setNewItem(event.target.value);
   }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log('cliccato')
+  }
+
   return (
     <>
       <h1>panetteria stock</h1>
       <ul>
-        {articles.map((article, index) => <li key={index}>{article}</li>)}
+        {articles.map((article) => <li key={article.id}>{article.title}</li>)}
       </ul>
 
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <input type="text" onChange={handleChange} value={newItem} />
         <p>{newItem}</p>
         <button>add new articles</button>
